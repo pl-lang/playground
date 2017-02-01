9615a3c9a7795643db8b305661958b6d257eb5df
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2017-02-01 01:48:50 -0300

    ahora la ventana de ejecucion puede emitir eventos

--- src/Window.ts
+++ src/Window.ts
@@ -1,11 +1,14 @@
 import {S3, Interpreter, Value} from 'interprete-pl'
+import Emitter from './Emitter'
 import Prompt from './Prompt'
 import * as $ from 'jquery'
 
-export default class Window {
+export default class Window extends Emitter {
+    
     private interpreter: Interpreter
 
     constructor (readonly container: JQuery) {
+        super(['evaluation-error'])
         this.container.append($('<div class="line"></div>'))
     }
 
@@ -23,6 +26,12 @@ export default class Window {
         this.interpreter.on('write', v => this.write(v))
 
         this.interpreter.on('read', () => {this.read()})
+
+        this.interpreter.on('evaluation-error', (error_info) => {
+            this.container.append($('<br>'))
+            this.container.append($('<div class="line"><span>Programa finalizado debido a un error</span></div>'))
+            this.emit('evaluation-error', error_info)
+        })
 
         this.interpreter.on('program-finished', () => {
             this.container.append($('<br>'))
