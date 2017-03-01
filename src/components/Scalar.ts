7c8be6b7395e7692d844a723462d222f324b74c2
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2017-03-01 00:15:58 -0300

    incluir una manera de actualizar un Scalar

--- src/components/Scalar.ts
+++ src/components/Scalar.ts
@@ -5,15 +5,19 @@ export default class PanelToggler {
     private parent: JQuery
     private remove_button: JQuery
     private container: JQuery
+    private value_element: JQuery
+    name: string
 
     constructor(parent: JQuery, name: string, value: number | string) {
         this.parent = parent
+
+        this.name = name
 
         this.container = $('<pre class="scalar flex-row"></pre>')
 
         const name_element = $(`<span class="scalar-name">${name}:</span>`)
 
-        const value_element = $(`<span class="value">${value}</span>`)
+        this.value_element = $(`<span class="value">${value}</span>`)
 
         this.remove_button = $('<button class="simple-button-icon octicon octicon-x pull-right"></button>')
 
@@ -21,8 +25,12 @@ export default class PanelToggler {
             this.container.remove()
         })
 
-        this.container.append(name_element, value_element, this.remove_button)
+        this.container.append(name_element, this.value_element, this.remove_button)
 
         this.parent.append(this.container)
+    }
+
+    set_value(v: number | string) {
+        this.value_element.replaceWith($(`<span class="value">${v}</span>`))
     }
 }
