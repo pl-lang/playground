9008edd2115cc26b5886c86213bebc0a8fa6c3f0
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2017-07-15 01:52:27 -0300

    agregar descripcion a botones de ejecucion

--- src/components/OutputPanel.ts
+++ src/components/OutputPanel.ts
@@ -41,8 +41,8 @@ export default class OutputPanel implements Resizeable {
         const icon = $('<span style="margin-left:10px" class="octicon octicon-terminal"></span>')
         const title = $('<span class="title">SALIDA</span>')
 
-        this.step_button = $('<button class="green-button icon-button"><span class="button-icon octicon octicon-triangle-right"></span></button>')
-        this.stop_button = $('<button class="red-button icon-button"><span class="button-icon octicon octicon-primitive-square"></span></button>')
+        this.step_button = $('<button title="Dar un paso" class="green-button icon-button"><span class="button-icon octicon octicon-triangle-right"></span></button>')
+        this.stop_button = $('<button title="Detener ejecución" class="red-button icon-button"><span class="button-icon octicon octicon-primitive-square"></span></button>')
 
         bar.append(icon, title, this.step_button, this.stop_button)
 
